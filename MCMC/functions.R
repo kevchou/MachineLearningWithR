9d773d4d05c51aa8ba816cdd932c4cb9aee78d71
Kevin Chou <kevchou@gmail.com>
2014-04-11 18:53:06 -0400

    cleaned some code

--- MCMC/functions.R
+++ MCMC/functions.R
@@ -1,41 +1,80 @@
-library(stringr)
-library(ggplot2)
-library(reshape2)
+library(stringr)  # For string manipulation
+library(ggplot2)  # Plots
+library(reshape2) # Reshaping data tables
 
+CleanText <- function(x) {
+    ## Removes all punctuation, numbers, multiple spaces from a string input
+    ##
+    ## Args:
+    ##   x: a string
+    ##
+    ## Returns:
+    ##   Cleaned up string with no numbers and symbols
 
-## Removes all punctuation, numbers, multiple spaces from a string input
-textonly <- function(x) {
     x <- str_replace_all(x, "[[:punct:]]", "")   # Removes all symbols
     x <- str_replace_all(x, "[[:digit:]]", "")   # Removes all numbers
+
+    ## Removes multiple spaces
     x2 <- str_replace_all(x, "  ", " ")
     while (x2 != x) {
         x <- x2
         x2 <- str_replace_all(x2, "  ", " ")
     }
-    x2
+
+    return(x2)
 }
 
-## Decrypts or encrypts a string using a specified mapping
-## decipher=TRUE: input an ENCRYPTED string, outputs decrypted string using mapping
-## decipher=FALSE: input an UNENCRYPTED string, encrypts it using specified mapping
-decipher <- function(input, mapping, decipher=TRUE) {
+CipherText <- function(input, mapping, decipher=TRUE) {
+    ## Decrypts or encrypts a string using a simple substitution cipher with
+    ## inputted mapping
+    ##
+    ## Args:
+    ##   input: the string to decrypt/encrypt
+    ##   mapping: mapping to use
+    ##   decipher: if TRUE, then dencrypts input string. False: encrypts string
+    ##
+    ## Returns:
+    ##   The encrypted/decrypted string
+
     mapping <- paste(mapping, collapse="")
-    letter <- paste(toupper(letters), collapse="")
+
+    letter <- paste(toupper(letters), collapse="") # Capitalize everything
+
     input <- toupper(input)
-    if (decipher) { output <- chartr(mapping, letter, input) }
-    else {output <- chartr(letter, mapping, input)}
-    output
+
+    ## Encrypt or decrypt
+    if (decipher) {
+        output <- chartr(mapping, letter, input)
+    } else {
+        output <- chartr(letter, mapping, input)
+    }
+
+    return(output)
 }
 
-## Calculates Score using specified mapping
-score <- function(string_input, mapping) {
-    string_deciphered <- decipher(string_input, mapping)
-    temp <- embed(unlist(strsplit(toupper(string_deciphered), '')), 2)
+
+CalcScore <- function(string_input, mapping) {
+    ## Calculates current Score with current mapping
+    ##
+    ## Args:
+    ##   string_input: The current string
+    ##   mapping: current mapping used to decrypt
+    ##
+    ## Returns:
+    ##   The current score
+
+    string_deciphered <- CipherText(string_input, mapping)
+
+    ## Create table of consecutive letters
+    temp <- embed(unlist(strsplit(toupper(string_deciphered), split='')), 2)
     temp.count <- table( temp[,2], temp[,1])
     temp.melt <- melt(temp.count)
 
-    let <- c(' ', toupper(letters) )
-    score= 0
+    let <- c(' ', toupper(letters) )    # All letters
+
+    score <- 0
+
+    ## Calculates score
     for (i in let) {
         for (j in let) {
             r.ij <- pair.count$value[which(pair.count$Var1==i & pair.count$Var2==j)] + 1
@@ -44,233 +83,93 @@ score <- function(string_input, mapping) {
             score = score + fx.ij*log(r.ij)
         }
     }
-    score
+
+    return(score)
 }
 
-## Attempts to decrypt simple substitution cipher using MCMC
-## inputs: encrypted string
-## p = scaling parameter (tempering)
-## iterations = max no. of iterations
-## start.map= mapping to start with (random by default)
-run.mcmc.decipher <- function(to_decipher, p=1, iterations=2000, start.map = sample(toupper(letters)) ) {
-    mapping <- start.map
+RunMcmcDecipher <- function(to.decipher,
+                            p = 1,
+                            iterations = 2000,
+                            start.map = sample(toupper(letters))) {
+    ## Uses the Metropolis algorithm to decrypt a simple substitution cipher
+    ##
+    ## Args:
+    ##   to_decipher: the text to decrypt
+    ##   p: scaling (tempering) parameter
+    ##   iterations: number of iterations to run for MCMC
+    ##   start.map: letter mapping to start with. Random by default
+    ##
+    ## Returns:
+    ##   a list object containing:
+    ##     - The decrypted text (if successful)
+    ##     - Mapping used for the decryption
+    ##     - table of scores
 
-    current.decipher <- decipher(to_decipher, mapping)
-    current.score <- score(to_decipher, mapping)
+    current.mapping  <- start.map
+    current.decipher <- CipherText(to.decipher, current.mapping)
+    current.score    <- CalcScore(to.decipher, current.mapping)
 
     max.decipher <- current.decipher
-    max.score <- current.score
+    max.score    <- current.score
+
+    ## To keep track of all scores
+    all.scores <- rep(NA, iterations)
+    text.history <- rep(NA, iterations)
+    num.accept <- 0
 
     i <- 1
-    numaccept <- 0
-    list <- NULL
-    while( i <= iterations) {
-                                        # Randomly switch 2 letters in the mapping
+
+    while(i <= iterations) {
+
+        ## Randomly switch 2 letters in the mapping
         proposal <- sample(1:26, 2)
-        prop.mapping <- mapping
-        prop.mapping[proposal[1]] = mapping[proposal[2]]
-        prop.mapping[proposal[2]] = mapping[proposal[1]]
+        prop.mapping <- current.mapping
+        prop.mapping[proposal[1]] <- current.mapping[proposal[2]]
+        prop.mapping[proposal[2]] <- current.mapping[proposal[1]]
 
-        prop.score <- score(to_decipher, prop.mapping)
+        ## Calculate score using new mapping
+        prop.score <- CalcScore(to.decipher, prop.mapping)
 
-        if( log(runif(1)) < p*(prop.score - current.score) ) {
-            mapping = prop.mapping
-            current.score <- prop.score
-            current.decipher <- decipher(to_decipher, mapping)
+        if(log(runif(1)) < p*(prop.score - current.score)) {
 
-            if( current.score > max.score) {
-                max.score = current.score
-                max.mapping = mapping
+            ## If new mapping is accepted, update current variables
+            current.mapping  <- prop.mapping
+            current.score    <- prop.score
+            current.decipher <- CipherText(to.decipher, current.mapping)
+
+            ## If new score is higher than previous max score, update.
+            if(current.score > max.score) {
+                max.score   <- current.score
+                max.mapping <- current.mapping
             }
-            numaccept <- numaccept+1
+
+            ## Increment number of accepted
+            num.accept <- num.accept + 1
         }
-        cat(i, substring(current.decipher,1,50), current.score, '\n')
-        if (i %% 100 ==0 ) {
-            list <- rbind(list, c(i, substring(current.decipher, 1, 70), current.score))
+
+        if (i %% (iterations/100) == 0) {
+            cat(i, substring(current.decipher,1,57), current.score, '\n')
         }
-        i=i+1
+
+        all.scores[i]   <- current.score
+        text.history[i] <- substring(current.decipher, 1, 57)
+
+        i <- i + 1
     }
-    ret <- list(max.score = max.score, max.mapping = max.mapping, list=list, numaccept= numaccept)
+
+    ret <- list(max.score = max.score,
+                max.mapping = max.mapping,
+                scores = all.scores,
+                text.history = text.history,
+                num.accept= num.accept)
+
     return(ret)
 }
 
-## Splits a string into "period" substrings
-text.split <- function(x, period) {
-    x <- unlist(strsplit(x," "))  # Splits string into words
-    string = matrix(0, nrow=ceiling(length(x)/period) ,ncol=period)
 
-    for ( i in 1:ceiling(length(x)/period)) {
-        temp <- NULL
-        for (j in (period*(i-1) +1) :(i*period)) {
-            if ( is.na(x[j])) { x[j] = ""}
-            temp <- c(temp, x[j])
-        }
-        string[i,] = temp
-    }
-    out <- list()
-    for (i in 1:period) {
-        out[[i]] <- paste(string[,i], collapse=" ")
-    }
-    unlist(out)
-}
 
-## Decrypts substitution cipher with different mapping every period
-decipher2 <- function(input.string, map.list, period, ...) {
-    string.split <- text.split(input.string, period)
-    deciphered <- list()
-    for (i in 1:period) {
-        deciphered[[i]] <- decipher(string.split[i], map.list[[i]], ...)
-        deciphered[[i]] <- unlist(strsplit(deciphered[[i]], " "))
-    }
 
-    if (period > 1) {
-        n <- length(deciphered[[1]])
-        for (j in 2:period) {
-            if (length(deciphered[[j]]) < n) { deciphered[[j]] <- c(deciphered[[j]], "")}
-        }
-    }
-    recombine.mat <- NULL
-    for (i in 1:period) {
-        recombine.mat <- cbind(recombine.mat, deciphered[[i]])
-    }
-    recombined <- paste(as.vector(t(recombine.mat)), collapse=" ")
-    textonly(recombined)
-}
 
-## Calculates score of each substring, and adds them up
-score2 <- function( input, maplist, period) {
-    input.split <- text.split(input, period)
-    score.list <- NULL
-    score.sum <- 0
-    for (i in 1:period) {
-        score.list[i] <- score(input.split[i], maplist[[i]])
-        score.sum <- score.sum + score.list[[i]]
-    }
-    score.sum
-}
-
-## MCMC decipher with periods
-run.mcmc.decipher2 <- function(to_decipher, period, p=1, iterations=2000) {
-    map.list <- list()
-    for (i in 1:period){
-        map.list[[i]] <- sample(toupper(letters))
-    }
-
-    current.decipher <- decipher2(to_decipher, map.list, period)
-    current.score <- score2(to_decipher, map.list, period)
-
-    max.decipher <- current.decipher
-    max.score <- current.score
-
-    i <- 0
-    numaccept <- 0
-    list <- list()
-    while( i <= iterations) {
-        ## Randomly switch 2 letters in the mapping
-        proposal.list <- list()
-        prop.mapping.list = map.list
-        for (j in 1:period){
-            proposal.list[[j]] <- sample(1:26, 2)
-            prop.mapping.list[[j]][proposal.list[[j]][1]] = map.list[[j]][proposal.list[[j]][2]]
-            prop.mapping.list[[j]][proposal.list[[j]][2]] = map.list[[j]][proposal.list[[j]][1]]
-        }
-        prop.score <- score2(to_decipher, prop.mapping.list, period)
-
-        if( log(runif(1)) < p*(prop.score - current.score)) {
-            map.list = prop.mapping.list
-            current.score <- prop.score
-            current.decipher <- decipher2(to_decipher, map.list, period)
-
-            if( current.score > max.score) {
-                max.score = current.score
-                max.mapping.list = prop.mapping.list
-            }
-            numaccept = numaccept + 1
-        }
-
-        if (i %% 100 ==0 ) {
-            accuracy.count <- rep(0,period)
-            for (n in 1:period) {
-                accuracy.count[n] <- sum(map.list[[n]] == starting.map[[n]])
-            }
-            accuracy <- accuracy.count/26
-            list <- rbind(list, c(i, substring(current.decipher, 1, 70), current.score, accuracy))
-            colnames(list) <- c('iteration', 'decoded string', 'score', rep('accuarcy', period))
-        }
-        cat(i, substring(current.decipher,1,50), current.score, accuracy, '\n')
-        i=i+1
-    }
-    ret <- list(max.score = max.score, start.mapping = starting.map, max.mapping.list = max.mapping.list, list=list, numaccept = numaccept)
-    return(ret)
-}
-
-## MCMC decipher with periods, using random scan updating 1 map at a time
-run.mcmc.decipher.random <- function(to_decipher, period, p=1, iterations=2000) {
-
-    map.list <- list()
-    for (i in 1:period){
-        map.list[[i]] <- sample(toupper(letters))
-    }
-
-    current.decipher <- decipher2(to_decipher, map.list, period)
-    current.score <- score2(to_decipher, map.list, period)
-
-    max.decipher <- current.decipher
-    max.score <- current.score
-
-    i <- 0
-    numaccept <- 0
-    list <- list()
-    while( i <= iterations) {
-        ## Randomly switch 2 letters in the mapping
-        ## random scan through no. of periods
-        prop.mapping.list = map.list
-        coord <- floor(runif(1,0,period)) + 1
-        proposal.switch <- sample(1:26, 2)
-        prop.mapping.list[[coord]][proposal.switch[1]] = map.list[[coord]][proposal.switch[2]]
-        prop.mapping.list[[coord]][proposal.switch[2]] = map.list[[coord]][proposal.switch[1]]
-
-        prop.score <- score2(to_decipher, prop.mapping.list, period)
-
-        if( log(runif(1)) < p*(prop.score - current.score)) {
-            map.list = prop.mapping.list
-            current.score <- prop.score
-            current.decipher <- decipher2(to_decipher, map.list, period)
-
-            if( current.score > max.score) {
-                max.score = current.score
-                max.mapping.list = prop.mapping.list
-            }
-            numaccept = numaccept + 1
-        }
-
-        ## Saves information every 100 iterations
-        if (i %% 100 ==0 ) {
-            accuracy.count <- rep(0,period)
-            for (n in 1:period) {
-                accuracy.count[n] <- sum(map.list[[n]] == starting.map[[n]])
-            }
-            accuracy <- accuracy.count/26
-            list <- rbind(list, c(i, substring(current.decipher, 1, 60), current.score, accuracy))
-        }
-
-        ## Prints out current iteration info
-        cat(i, substring(current.decipher,1,60), current.score, accuracy, '\n')
-        i=i+1
-    }
-    colnames(list) <- c('iteration', 'decoded string', 'score', rep('accuarcy', period))
-    ret <- list(max.score = max.score, start.mapping = starting.map, max.mapping.list = max.mapping.list, list=list, numaccept = numaccept)
-    return(ret)
-}
-
-                                        ## Creates 'period' random mappings
-create.map <- function(period) {
-    map.list <- list()
-    for (i in 1:period) {
-        map.list[[i]] <- sample(toupper(letters))
-    }
-    map.list
-}
 
 ## Unigram attack. replaces most frequent letter in encrypted text with most frequent
 ## letter in reference text, etc
@@ -294,22 +193,7 @@ unigram <- function(test) {
     for (i in 1:length(letters)) {
         mapping[i] = names(coded.sorted2)[which(names(let.sorted) == toupper(letters)[i])]
     }
-    new.test<- decipher(test, mapping)
+    new.test<- CipherText(test, mapping)
     ret <- list(deciphered = new.test, mapping = mapping)
     ret
 }
-
-
-## Set up War and Peace for reference text
-readfile <- readLines('warandpeace.txt')            # Read in War and Peace text file
-training <- toupper(paste(readfile, collapse=' '))  # Collapses it to one line
-training <- textonly(training)
-
-## Creates a table of number of times (row letter) is followed by (column letter)
-let <- unlist(strsplit(training, ''))
-let2 <- embed(let, 2)
-mat.count <- table( let2[,2], let2[,1] )
-pair.count <- melt(mat.count)
-
-## Divides each number in a row by its row sum. Frequency
-mat.trans <- sweep( mat.count, 1, rowSums(mat.count), FUN='/')
--- MCMC/main.R
+++ MCMC/main.R
@@ -1,14 +1,47 @@
 source('functions.R')
 
+## Set up War and Peace for reference text
+readfile <- readLines('warandpeace.txt')            # Read in War and Peace text file
+training <- toupper(paste(readfile, collapse=' '))  # Collapses it to one line
+training <- CleanText(training)
+
+## Creates a table of number of times (row letter) is followed by (column letter)
+let <- unlist(strsplit(training, ''))
+let2 <- embed(let, 2)
+mat.count <- table( let2[,2], let2[,1] )
+pair.count <- melt(mat.count)
+
+## Divides each number in a row by its row sum. Frequency
+mat.trans <- sweep( mat.count, 1, rowSums(mat.count), FUN='/')
+
+## Plots
+tab <- melt(mat.trans)
+names(tab) <- c('cond1', 'prob2', 'prob')
+ggplot(tab, aes(cond1,prob2)) +
+    geom_tile(aes(fill=prob)) +
+    scale_fill_gradient(low="white", high="black", limits=c(0,1)) +
+    labs(title="Using \'War and Peace\'", x="Conditional on first letter",
+         y="Probability of second letter", prob="Probability")+
+    coord_equal()
+
+
+## Reading Flatline into R
 x <- readLines('flatland.txt')          # Loads Flatland into R
 x <- toupper(paste(x, collapse=' '))    # Capitalizes everything
-x <- textonly(x)                        # Make sure text is in proper format
-x <- substring(x, 2, nchar(x))
+x <- CleanText(x)                        # Make sure text is in proper format
+x <- substring(x, 2, nchar(x))          # Removes first character
 
-# SET UP
-period = 1                              # Number of different ciphers to use
-starting.map <- create.map(period)      # Create a cipher map
-y <- decipher2(x, starting.map, period=period, decipher=F) # Encrypts text
 
-## Start MCMC
-decrypted.text <- run.mcmc.decipher.random(y, period=period, p=1, iterations=5000)
+starting.map <- sample(toupper(letters)) # Random letter mapping to start with
+
+encrypted.text <- CipherText(x, starting.map, decipher = FALSE)
+
+decrypted.text1 <- RunMcmcDecipher(encrypted.text, p = 0.1, iterations = 5000)
+decrypted.text2 <- RunMcmcDecipher(encrypted.text, p = 1,   iterations = 5000)
+decrypted.text3 <- RunMcmcDecipher(encrypted.text, p = 10,  iterations = 5000)
+
+
+everything <- rbind(
+    data.frame(p = "p=0.1", iteration = c(1:5000), score = decrypted.text1$scores),
+    data.frame(p = "p=1",   iteration = c(1:5000), score = decrypted.text2$scores),
+    data.frame(p = "p=10",  iteration = c(1:5000), score = decrypted.text3$scores))
